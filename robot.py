4839d9fe802625364b645643eb77ba227e3ccb10
Hao Lin <guoguolin31@gmail.com>
2026-05-20 23:44:25 -0400

    Robot Project
    The first version of the robot project with basic functions, where the robot can pick up an item if the conditions are satisfied.

--- robot.py
+++ robot.py
@@ -1,11 +1,9 @@
 # robot.py
 
 
-import math
 from shapes import draw_disk
 import matplotlib.pyplot as plt
 import copy
-import numpy as np
 
 
 class Robot:
@@ -15,15 +13,8 @@ class Robot:
     A robot moves in the cardinal directions only--north, east, south, west (NESW)
     --and moves one unit distance in each time step.
 
-    The Robot class also has a class attribute `color`.
-
-    Class attribute:
-    -----------------
-    
-    color : str; Robot's color. A regular `Robot` is blue
-
-    Instance Attributes:
-    ---------------------
+    Attributes:
+    ------------
 
     _id_: int; the robot's identifier
 
@@ -37,9 +28,6 @@ class Robot:
     _items_picked: list; each element of the list is an Item that the robot has 
     picked up. The list is initially empty
     """
-
-    # Class attribute
-    color = 'b'
 
 
     def __init__(self, id_, max_weight, total_time, init_loc):
@@ -62,14 +50,16 @@ class Robot:
         self._max_weight = max_weight
         self._total_time = total_time
         self._init_loc = init_loc
-        self._items_picked = []
+        self._items_picked=[]
         
+
 
     def get_id(self):
         """
         Returns (int) the `_id_` of the robot.
         """
         return self._id_
+        
 
 
     def get_items_picked(self):
@@ -77,53 +67,65 @@ class Robot:
         Returns a deep copy of `_items_picked`.
         """
         return copy.deepcopy(self._items_picked)
-
+        
+   
 
     def total_operation_time(self):
         """
         Returns the total operation time of the robot immediately after 
-        completing its most recent Item pick-up. If no item has been picked 
-        up, the total operation time is 0.
+        completing its most recent Item pick-up. The return type is int.
+
+        You may find it useful to leverage the `_items_picked_` attribute. If 
+        no item has been picked up, return 0 as the total operation time.
         """
-        if len(self._items_picked) == 0:
+
+
+        if len(self._items_picked)<1:
             return 0
-        last_item = self._items_picked[-1]
-        return last_item.picked_window.right
-    
+        if len(self._items_picked)>=1:
+            return self._items_picked[-1].picked_window.right
 
     def latest_resting_loc(self):
         """
-        Returns the latest resting location of the robot. If no item has been 
-        picked up, the robot's latest resting location is just its initial 
-        location; otherwise, the robot's latest resting location is where it 
-        picked up the most recent item.
+        Returns the latest resting location of the robot. This should be a 
+        length 2 list representing a location (one x-coord and one y-coord).
+
+        You may find it useful to leverage the `_items_picked_` attribute.
+
+        If no item has been picked up, the robot's latest resting location 
+        is just its initial location; otherwise, the robot's latest resting
+        location is where it picked up the most recent item.
         """
-        if len(self._items_picked) == 0:
+        
+        if len(self._items_picked)<1:
             return self._init_loc
-        last_item = self._items_picked[-1]
-        return last_item.loc
+        if len(self._items_picked)>=1:
+            return self._items_picked[-1].loc
+        
     
 
     def draw(self, loc):
         """
-        Draw a circle of diameter 1 to represent the robot at a given
-        location `loc`. Label the circle at the center with its `_id_`.
-        The color of the circle should match the `color` class attribute 
-        of the robot.
+        Draw a blue circle of diameter 1 to represent the robot at a given
+        location `loc`. Label the circle at the center with the robot's `_id_`.
         
         Assumes figure window is already open.
         
         Parameter:
         -----------
         
-        loc: list; a length-2 list representing the location where the robot 
-        should be drawn.
+        loc: list; a length 2 list storing the x- and y-coordinates at which 
+        the robot should be drawn.
         """
-        r = 0.5
-        draw_disk(loc[0], loc[1], r, self.color)
-        plt.text(loc[0], loc[1], str(self._id_), horizontalalignment='center')
+        #Display the plot
+        plt.plot()
+        #draw
+        s=str(self._id_)
+        plt.text(loc[0],loc[1],s)
+        draw_disk(loc[0],loc[1],0.5,'blue')
+        
 
-    
+
     def travel_steps(self, curr, dest):
         """
         Returns a valid list of locations that form a path between two locations,
@@ -140,37 +142,45 @@ class Robot:
         Parameters:
         -----------
 
-        curr: list; a length-2 list storing the robot's current x-y coordinate
+        curr: list; a length 2 list storing the robot's current x-y coordinate
 
-        dest: list; a length-2 list storing the robot's destiny x-y coordinate
+        dest: list; a length 2 list storing the robot's destiny x-y coordinate
         
-        If `curr` and `dest` refer to the same location, return a length-1 list 
-        containing this shared location.
+        If `curr` and `dest` refer to the same location, return a length 1 list 
+        storing this shared location.
+
         """
-        # Set up the old and new x and y positions
-        steps = []
-        x1 = curr[0]
-        y1 = curr[1]
-        x2 = dest[0]
-        y2 = dest[1]
+        loc=[curr]
+        #move along x-direction
+        if curr[0]<dest[0]:           #the current x position is at the left of 
+                                      #the destiny x position
+            newx=curr[0]              
+            while newx<dest[0]:
+                newx+=1
+                loc.append([newx,curr[1]])  #update location and store it to list
+        if curr[0]>dest[0]:           #the current x position is at the right of 
+                                      #the destiny x position
+            newx=curr[0]
+            while newx>dest[0]:
+                newx-=1
+                loc.append([newx,curr[1]])  #update location and store it to list
+                
+        #move along y-direcction
+        if curr[1]<dest[1]:           #the current y position is below  
+                                      #the destiny y position
+            newy=curr[1]
+            while newy<dest[1]:
+                newy+=1
+                loc.append([newx,newy])  #update location and store it to list
+        if curr[1]>dest[1]:           #the current y position is above  
+                                      #the destiny y position
+            newy=curr[1]
+            while newy>dest[1]:
+                newy-=1
+                loc.append([newx,newy])  #update location and store it to list
+                
+        return loc
 
-        # Move along the x-direction
-        delta_x = x2 - x1
-        if not np.isclose(delta_x, 0):
-            sign = int(delta_x / abs(delta_x))
-            for x in range(x1, x2 + sign, sign):
-                steps.append([x, y1])
-        else:
-            steps.append(curr)
-        
-        # Move along the y-direction
-        delta_y = y2 - y1
-        if not np.isclose(delta_y, 0):
-            sign = int(delta_y / abs(delta_y))
-            for y in range(y1 + sign, y2 + sign, sign):
-                steps.append([x2, y])
-        return steps
-    
 
     def pick(self, item, do_pick=True, num_arms=0):
         """
@@ -179,7 +189,15 @@ class Robot:
         If the robot is able to pick up the item, execute the pick-up if `do_pick`
         evaluates to True.
         
-        Details are the same as in Project 5.
+        The robot is able to pick up `item` when the following are true:
+        1. the robot's physical characteristics allows it to pick up `item`
+        2. the robot can travel to `item` and fully pick it up in time
+        3. `item` has not yet been scheduled for pickup
+        
+        If the pick-up is executed, we need to:
+        1. Update `item`'s picked_window
+        2. Update `Robot`'s `_items_picked` attribute
+        No attributes should be updated if the pick-up is not executed.
 
         Parameters:
         -----------
@@ -190,29 +208,50 @@ class Robot:
         it be possible. Default is True.
 
         num_arms: int; the number of arms the robot has. Default is 0.
+
         """
-        # Check if the robot can pick up the item
-        robot_capable = item.valid_pickup(self._max_weight, num_arms)
-        available_time = self._total_time - self.total_operation_time()
-        curr = self.latest_resting_loc()
-        dest = item.loc
-        steps = self.travel_steps(curr, dest)
-        travel_time = len(steps) - 1
-        required_time = travel_time + item.duration
-        have_time = required_time <= available_time
-        item_waiting = item.picked_window is None
-        success = robot_capable and have_time and item_waiting
+        # Condition 1: does the robot's physical chacteristics allow it to pick up
+        # the item? Hint: you wrote a relevant function in the Item class.
 
-        # Pick up the item if possible and required
-        if success and do_pick:
-            start_time = self.total_operation_time()
-            pick_time = start_time + travel_time
-            item.update_pickup_status(pick_time)
+        if item.valid_pickup(self._max_weight, num_arms)==True:
+            success=True
+        else: 
+            success = False
+            return success
+
+        # Condition 2: can the robot travel to the item and pick it up within the 
+        # given total simulation time `_total_time`?
+        # Hint: you may find the total_operation_time(), latest_resting_loc(),
+        # and travel_steps() methods useful.
+        path=self.travel_steps(self.latest_resting_loc(),item.loc)
+        travel_time=len(path)-1
+        start_pickup_time=int(self.total_operation_time()+travel_time)
+        end_pickup_time=int(start_pickup_time+item.duration)
+        if end_pickup_time<self._total_time:
+            success=True
+        else:
+            success = False
+            return success
+        # Condition 3: is the item not already scheduled for pick-up?
+        # Hint: `item` has a relevant attribute that you may need.
+
+        if item.picked_window==None:
+            success = True
+        else:
+            success = False
+            return success
+        # If all three conditions above are met, set `success` to True.
+      
+        # If the robot is able to pick up the item and `do_pick` is True,
+        # we execute the pick-up by
+        # (1) Updating the item's `picked_window`
+        # (2) Updating the robot's `_items_picked`
+        if do_pick==True:
+            item.update_pickup_status(start_pickup_time)
             self._items_picked.append(item)
-
         # Return `success`
         return success
-    
+
 
     def get_location(self, t):
         """
@@ -221,7 +260,7 @@ class Robot:
 
         This method calls the total_operation_time(), latest_resting_loc(), and
         travel_steps() methods that you implemented and assumes correctness in
-        these methods. 
+        those methods. 
 
         Read but DO NOT modify the code.
         """
@@ -245,231 +284,4 @@ class Robot:
         dest = self._items_picked[index].loc
         steps = self.travel_steps(curr, dest)
         return steps[time_offset]
-
-
-class FastRobot(Robot):
-    """
-    A `FastRobot` is a `Robot`. A `FastRobot` can move at most `_speed_multiplier`
-    distance in one timestep. A `FastRobot` is green ('g').
     
-    Class attribute:
-    -----------------
-    
-    color: str; FastRobot's color. A `FastRobot` is green
-        
-    Instance attributes:
-    ---------------------
-
-    _speed_multiplier: float; the distance that a `FastRobot` is able to move 
-    in one time step
-    """
-    
-    # Class attribute
-    color = 'g'
-    
-
-    def __init__(self, id_, max_weight, total_time, init_loc,
-                 speed_multiplier=np.random.uniform(1.0, 7.0)):
-        """
-        Construct a `FastRobot` with a speed multiplier.
-        """
-        self._speed_multiplier=speed_multiplier
-        super().__init__(id_, max_weight, total_time, init_loc)
-
-        
-    def travel_steps(self, curr, dest):
-        """
-        Returns a valid list of locations that form a path between two locations,
-        `curr` and `dest`. As for a regular Robot, each location in the list 
-        corresponds to one time step. The first location in the returned list 
-        should be `curr`; the last location in the list should be `dest`.
-
-        Importantly, the FastRobot can move `_speed_multiplier` distance in one 
-        timestep. Also, it can only move in one direction once it starts moving,
-        meaning it can only move in the straight line path from `curr` to `dest`.
-
-        Parameters:
-        -----------
-
-        curr: list; a length-2 list storing the robot's current x-y coordinate
-
-        dest: list; a length-2 list storing the robot's destiny x-y coordinate
-        
-        If `curr` and `dest` refer to the same location, return a length-1 list 
-        containing this shared location.
-
-        Example 1: If a `FastRobot` with speed multiplier of 3 is to go from
-        [0, 0] to [4, 0], then the list of locations is [[0, 0], [3, 0], [4, 0]]
-        Example 2: If a 'FastRobot' with speed multiplier of sqrt(2) is to go from
-        [0, 0] to [2, 2], then the list of locations is [[0, 0], [1, 1], [2, 2]]
-        Example 3: If a 'FastRobot' with speed multiplier of 6 is to go from
-        [0, 0] to [3, 4], then the list of locations is [[0, 0], [3, 4]]
-        """
-        lis=[curr]                                  #initialization
-        k=1
-        speed=self._speed_multiplier
-        
-        if curr[0]==dest[0] and curr[1]==dest[1]:   #angle calculation
-            return lis         
-        else:
-            angle=np.arctan2(dest[1]-curr[1],dest[0]-curr[0])
-            
-        while k>0:                                  #loop until destination is 
-            x=lis[-1][0]                            ##is reached
-            y=lis[-1][1]
-            x_dis=dest[0]-x
-            y_dis=dest[1]-y
-            distance=math.sqrt(x_dis**2+y_dis**2)
-            if  speed>= distance:              #destination reached
-                lis.append(dest)
-                return lis
-            else:
-                x_curr=x+speed*math.cos(angle)
-                y_curr=y+speed*math.sin(angle)
-                lis.append([x_curr,y_curr])         #update location list
-            
-class LimbedRobot(Robot):
-    """
-    A `LimbedRobot` is a `Robot` that has arm capabilities and can lift
-    heavier items. A `LimbedRobot` can only move `_slowdown_multiplier`
-    distance in one timestep. A `LimbedRobot` is magenta ('m')
-    
-    Class attribute:
-    -----------------
-    
-    color: str; LimbedRobot's color.  A `LimbedRobot` is magenta
-        
-    Instance attributes:
-    ---------------------
-
-    _num_arms: int; number of arms the `LimbedRobot` has
-
-    _slowdown_multiplier: float; a `LimbedRobot` can move `_slowdown_multiplier` 
-    units in one timestep. Assume this is in the range (0, 1)
-    """
-    
-    # Class attribute
-    color = 'm'
-
-
-    def __init__(self, id_, max_weight, total_time, init_loc, num_arms,
-                 slowdown_multiplier, max_load_factor=np.random.randint(2, 7)):
-        """
-        Construct a `LimbedRobot` that has arms, can carry heavier items, but
-        moves at a slower speed.
-
-        Parameters:
-        ------------
-
-        num_arms: int; corresponds to the `_num_arms` attribute
-
-        slowdown_multiplier: float; corresponds to the `_slowdown_multiplier` 
-        attribute
-
-        max_load_factor: int; the value to use to adjust the `_max_weight` of 
-        a `LimbedRobot`.  This parameter defaults to a random integer between 2 
-        and 7.  The actual `_max_weight` of a `LimbedRobot` is
-            max_weight * max_load_factor
-        """
-        super().__init__(id_, max_weight, total_time, init_loc)
-        self._num_arms=num_arms
-        self._slowdown_multiplier=slowdown_multiplier
-        self._max_weight=max_weight*max_load_factor
-
-
-    def travel_steps(self, curr, dest):
-        """
-        Returns a valid list of locations that form a path between two locations,
-        `curr` and `dest`. As for a regular Robot, each location in the list 
-        corresponds to one time step. The first location in the returned list 
-        should be `curr`; the last location in the list should be `dest`.
-
-        A `LimbedRobot` can only move `_slowdown_multiplier` distance in one time
-        step and can only move in either the x or the y direction in one step.
-
-        While there are multiple ways to construct the path, one simple solution
-        is to have the robot first move along the x-direction, then along the
-        y-direction.
-
-        Parameters:
-        -----------
-
-        curr: list; a length-2 list storing the robot's current x-y coordinate
-
-        dest: list; a length-2 list storing the robot's destiny x-y coordinate
-        
-        If `curr` and `dest` refer to the same location, return a length-1 list 
-        containing this shared location.
-        """
-        # Set up the old and new x and y positions
-        
-        
-        lis= [curr]
-        x1 = curr[0]
-        y1 = curr[1]
-        x2 = dest[0]
-        y2 = dest[1]
-        stepsize=self._slowdown_multiplier
-        
-        #return if locations are the same
-        if x1==x2 and y1==y2:
-            return lis
-        
-        # Move along the x-direction
-        delta_x = x2 - x1
-        if x2>x1:
-            notend=True
-            while not np.isclose(delta_x, 0) and notend:
-                x1=x1+stepsize
-                delta_x = x2 - x1
-                if x1>=x2:
-                    notend=False
-                    lis.append([x2,y1])
-                else:
-                    lis.append([x1, y1])
-        elif x1>x2:
-            notend=True
-            while not np.isclose(delta_x, 0) and notend:
-                x1=x1-stepsize
-                delta_x = x2 - x1
-                if x2>=x1:
-                    notend=False
-                    lis.append([x2,y1])
-                else:
-                    lis.append([x1, y1])
-        
-        # Move along the y-direction
-        delta_y = y2 - y1
-        if y2>y1:
-            notend=True
-            while not np.isclose(delta_y, 0) and notend:
-                y1=y1+stepsize
-                delta_y = y2 - y1
-                if y1>=y2:
-                    notend=False
-                    lis.append([x2,y2])
-                else:
-                    lis.append([x2, y1]) 
-        elif y1>y2:
-            notend=True
-            while not np.isclose(delta_y, 0) and notend:
-                y1=y1-stepsize
-                delta_y = y2 - y1
-                if y2>=y1:
-                    notend=False
-                    lis.append([x2,y2])
-                else:
-                    lis.append([x2, y1])
-                
-        return lis
-    
-
-    def pick(self, item, do_pick=True):
-        """
-        Override `Robot`'s pick method.
-        
-        The only difference is that a `LimbedRobot` has some number of arms.
-        """
-        return super().pick(item,do_pick,self._num_arms)
-
-
